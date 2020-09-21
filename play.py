2ba999fc0b6f1a5f5053f0225a05d1bff1f65ea2
tixon1501 <53827514+tixon1501@users.noreply.github.com>
2020-09-21 23:41:20 +0300

    Add files via upload

--- play.py
+++ play.py
@@ -52,7 +52,6 @@ monsters = [
 
 ]
 
-hero = monster(1000, "hero", None, 100, set())
 
 root = Tk()
 root.geometry('1080x720')
@@ -95,14 +94,26 @@ maps = [
            "cave_rock": 1, "riches": 1}, None, {"dragon", "worm"}),
     sector("riches", load_image("riches.jpg"), {
            "underground_lake": 1, "treasure_cave": 1}, None, None),
-    sector("submarine", load_image("submarine.jpg"), {"water": 0, "control": 1}, None, None),
-    sector("control", load_image("control.jpg"), {"submarine": 1, "lake": 0, "uderwater_cave": 0}, None, None),
-    sector("underwater_cave", load_image("underwater_cave.jpg"), {"pit": 1, "control": 0}, None, True),
-    sector("pit", load_image("pit.jpg"), {"underwater_cave": 1, "big_pit": 0}, None, None),
+    sector("submarine", load_image("submarine.jpg"),
+           {"water": 0, "control": 1}, None, None),
+    sector("control", load_image("control.jpg"), {
+           "submarine": 1, "lake": 0, "uderwater_cave": 0}, None, None),
+    sector("underwater_cave", load_image("underwater_cave.jpg"),
+           {"pit": 1, "control": 0}, None, True),
+    sector("pit", load_image("pit.jpg"), {
+           "underwater_cave": 1, "big_pit": 0}, None, None),
     sector("big_pit", load_image("big_pit.jpg"), {"pit": 0}, None, True),
-    sector("lake", load_image("lake.jpg"), {"control": 0, "glade": 0}, None, True),
-    sector("glade", load_image("glade.jpg"), {"lake": 0, "forest": 0}, None, True),
-    sector("forest", load_image("forest.jpg"), {"glade": 0, "hollow": 1}, None, None)
+    sector("lake", load_image("lake.jpg"), {
+           "control": 0, "glade": 0}, None, True),
+    sector("glade", load_image("glade.jpg"), {
+           "lake": 0, "forest": 0}, None, True),
+    sector("forest", load_image("forest.jpg"), {
+           "glade": 0, "hollow": 1}, None, None),
+    sector("seaweed", load_image("seaweed.jpg"), {
+           "submarine": 1, "bunker": 1}, None, True),
+    sector("bunker", load_image("bunker.jpg"), {"seaweed": 1}, True, None),
+    sector("cave_control", load_image("cave_control.jpg"),
+           {"control": 0, "riches": 1}, None, True)
 ]
 sect = maps[0]
 set_image(sect.photo)
@@ -113,6 +124,7 @@ subjects = [
     subject("net", None, load_image("net.jpg"))
 ]
 
+hero = monster(1000, "hero", None, 100, {subjects[2]})
 
 def moving(event):  # перемещение
     print(event.x, event.y)
@@ -134,8 +146,8 @@ def moving(event):  # перемещение
     elif sect.destination == "water":
         if 300 < event.x < 750 and event.y < 100:
             sect = maps[0]
-        elif event.y > 600 and "key" in hero.subjects:
-            pass  # поподание в подводную лодку
+        elif event.y > 600 and subjects[2] in hero.subjects:
+            sect = maps[7]
         elif event.x > 750 and 600 > event.y > 400:
             sect = maps[3]
     elif sect.destination == "cave":
@@ -151,6 +163,32 @@ def moving(event):  # перемещение
     elif sect.destination == "underground_lake":
         if event.y > 600:
             sect = maps[4]
+        elif 330 < event.x < 510 and 310 < event.y < 450:
+            sect = maps[6]
+    elif sect.destination == "submarine":
+        if 390 < event.x < 520 and 200 < event.y < 280:
+            sect = maps[8]
+        elif event.y < 50:
+            sect = maps[2]
+        elif 820 < event.x < 880 and 340 < event.y < 410:
+            sect = maps[15]
+    elif sect.destination == "control":
+        if event.y > 600:
+            sect = maps[7]
+        elif 220 < event.x < 330 and 320 < event.y < 430:
+            sect = maps[17]
+        elif 340 < event.x < 430 and 320 < event.y < 430:
+            sect = maps[12]
+        elif 400 < event.x < 540 and 320 < event.y < 430:
+            sect = maps[10]
+    elif sect.destination == "seaweed":
+        if event.x < 100:
+            sect = maps[7]
+        elif event.y > 600:
+            sect = maps[16]
+    elif sect.destination == "bunker":
+        if event.y > 600:
+            sect = maps[15]
 
     set_image(sect.photo)
     x = 0
